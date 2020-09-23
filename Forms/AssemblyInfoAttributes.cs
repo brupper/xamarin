237e5bc503a7261af3e601e637f2a8d9ebb8a1a3
Adam Barath <adambarath@msn.com>
2020-09-23 07:51:35 +0200

    Add font icons  https://montemagno.com/xamarin-forms-custom-fonts-everywhere/

--- Forms/AssemblyInfoAttributes.cs
+++ Forms/AssemblyInfoAttributes.cs
@@ -1,5 +1,9 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FA-R")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FA-S")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "FA-B")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: Xamarin.Forms.Internals.Preserve(AllMembers = true)]
 
