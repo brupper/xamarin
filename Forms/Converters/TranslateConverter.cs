5de86fa13a9d3778065cef5a3f157b79a3bae8ca
Adam Barath <adambarath@msn.com>
2020-09-23 07:52:17 +0200

    Add converters

---
+++ Forms/Converters/TranslateConverter.cs
@@ -0,0 +1,52 @@
+﻿using MvvmCross;
+using MvvmCross.Localization;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Brupper.Forms.Converters
+{
+    //[ValueConversion(typeof(string), typeof(string))]
+    public class TranslateConverter : IValueConverter
+    {
+        public static TranslateConverter Instance { get; } = new TranslateConverter();
+
+        private readonly IMvxTextProvider textProvider;
+
+        public TranslateConverter()
+        {
+            textProvider = Mvx.IoCProvider.GetSingleton<IMvxTextProvider>();
+        }
+
+        public static void Init()
+        {
+            var time = DateTime.UtcNow;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            var text = (string)value;
+
+            var translation = textProvider.GetText(null, null, text);
+            //var translation = Labels.ResourceManager.GetString(text);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    $"Key '{text}' was not found in resources for culture '{(CultureInfo.CurrentUICulture.Name ?? "<???>")}'.");
+#else
+				translation = text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forms/MarkupExtensions/TranslateExtension.cs
@@ -0,0 +1,44 @@
+﻿using MvvmCross;
+using MvvmCross.Localization;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Brupper.Forms
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        private readonly IMvxTextProvider textProvider;
+
+        public string Text { get; set; }
+
+        public TranslateExtension()
+        {
+            textProvider = Mvx.IoCProvider.GetSingleton<IMvxTextProvider>();
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+            {
+                return string.Empty;
+            }
+
+            var translation = textProvider.GetText(null, null, Text);
+            //var translation = Labels.ResourceManager.GetString(Text);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    $"Key '{Text}' was not found in resources for culture '{(CultureInfo.CurrentUICulture.Name ?? "<???>")}'.");
+#else
+				translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+
+            return translation;
+        }
+    }
+}
