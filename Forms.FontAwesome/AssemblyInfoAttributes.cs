b12ed52ae7094218fcae6e3a2c7198b72550c14e
Adam Barath <adambarath@msn.com>
2020-10-03 08:16:50 +0200

    Migrate FontAwesome into standalone project

---
+++ Forms.FontAwesome/AssemblyInfoAttributes.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FA-R")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FA-S")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "FA-B")]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: Xamarin.Forms.Internals.Preserve(AllMembers = true)]
+
