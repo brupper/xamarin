b59dc30955bab6f1c632d4f074c281fd3ce6b0da
Adam Barath <adambarath@msn.com>
2020-11-10 06:43:26 +0100

    B2C updates

---
+++ Identity.B2C.Admin/Services/Interfaces/IUserGraphManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Graph;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Brupper.Identity.B2C
+{
+    public interface IUserGraphManager
+    {
+        Task<IEnumerable<User>> ListUsersAsync();
+        Task<User> GetUserByIdAsync(string userId);
+        Task<User> GetUserBySignInNameAsync(string userId);
+        Task DeleteUserByIdAsync(string userId);
+
+        Task<bool> IsLicenseAdministrator(string userId);
+    }
+}
