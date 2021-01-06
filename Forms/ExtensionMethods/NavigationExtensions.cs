7e32bd718edd6a81c022f7b1cc61630ff805fe88
Adam Barath <adambarath@msn.com>
2021-01-06 08:57:17 +0100

    Minor refact

--- Forms/ExtensionMethods/NavigationExtensions.cs
+++ Forms/ExtensionMethods/NavigationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using MvvmCross;
+﻿using Brupper.Forms.ViewModels;
+using MvvmCross;
 using MvvmCross.Localization;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
@@ -9,31 +10,31 @@ using System.Threading.Tasks;
 
 public static class NavigationExtensions
 {
-    //public static async Task<TResult> Navigate<TViewModel, TResult>(this ViewModelBase viewModel)
-    //    where TViewModel : IMvxViewModelResult<TResult>
-    //{
-    //    viewModel.CanViewDestroy = false;
+    public static async Task<TResult> Navigate<TViewModel, TResult>(this ISupportBrupperViewModel viewModel)
+        where TViewModel : IMvxViewModelResult<TResult>
+    {
+        viewModel.CanViewDestroy = false;
 
-    //    TResult result = await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<TViewModel, TResult>();
+        TResult result = await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<TViewModel, TResult>();
 
-    //    viewModel.CanViewDestroy = true;
+        viewModel.CanViewDestroy = true;
 
-    //    return result;
-    //}
+        return result;
+    }
 
-    //public static Task<bool> Navigate<TViewModel>(this ViewModelBase viewModel)
-    //    where TViewModel : IMvxViewModelResult<bool> 
-    //    => viewModel.Navigate<TViewModel, bool>();
+    public static Task<bool> Navigate<TViewModel>(this ISupportBrupperViewModel viewModel)
+        where TViewModel : IMvxViewModelResult<bool>
+        => viewModel.Navigate<TViewModel, bool>();
 
-    //public static async Task<TResult> Navigate<TViewModel, TParam, TResult>(this ViewModelBase viewModel, TParam parameter)
-    //    where TViewModel : IMvxViewModel<TParam, TResult>
-    //{
-    //    viewModel.CanViewDestroy = false;
+    public static async Task<TResult> Navigate<TViewModel, TParam, TResult>(this ISupportBrupperViewModel viewModel, TParam parameter)
+        where TViewModel : IMvxViewModel<TParam, TResult>
+    {
+        viewModel.CanViewDestroy = false;
 
-    //    TResult result = await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<TViewModel, TParam, TResult>(parameter);
+        TResult result = await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<TViewModel, TParam, TResult>(parameter);
 
-    //    viewModel.CanViewDestroy = true;
+        viewModel.CanViewDestroy = true;
 
-    //    return result;
-    //}
+        return result;
+    }
 }
