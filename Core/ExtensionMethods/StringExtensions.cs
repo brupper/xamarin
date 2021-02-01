dcbc87888e74e8a2f0339663b205111ffbefcb6b
Adam Barath <adambarath@msn.com>
2021-02-01 08:02:17 +0100

    diacritics extensions

--- Core/ExtensionMethods/StringExtensions.cs
+++ Core/ExtensionMethods/StringExtensions.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
 using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 
 public static class StringExtensions
@@ -23,5 +22,36 @@ public static class StringExtensions
         var invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
 
         return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+    }
+
+    private static char[] nonDiacritics = { 'a', 'e', 'i', 'o', 'ö', 'ú', 'ü' };
+    private static char[] diacritics = { 'á', 'é', 'í', 'ó', 'ő', 'ú', 'ű' };
+
+    public static string Remove(this string str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            for (int j = 0; j < diacritics.Length; j++)
+            {
+                if (str[i] == diacritics[j])
+                    str.Replace(diacritics[j], nonDiacritics[j]);
+            }
+        }
+        return str;
+    }
+
+    public static string RemoveDiacritics(this string stIn)
+    {
+        var stFormD = stIn.Normalize(NormalizationForm.FormD);
+        var sb = new StringBuilder();
+        for (int ich = 0; ich < stFormD.Length; ich++)
+        {
+            var uc = CharUnicodeInfo.GetUnicodeCategory(stFormD[ich]);
+            if (uc == UnicodeCategory.UppercaseLetter || uc == UnicodeCategory.LowercaseLetter || uc == UnicodeCategory.DashPunctuation)
+            {
+                sb.Append(stFormD[ich]);
+            }
+        }
+        return (sb.ToString().Normalize(NormalizationForm.FormC));
     }
 }
