5cbf7a116ade7f5bb74041b6cc0d794a3cffda9f
Adam Barath <adambarath@msn.com>
2024-12-22 16:11:27 +0100

    add smartbreadcrumbs project

---
+++ Core/Models/LabelTranslations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Brupper;
+
+/// <summary> </summary>
+[System.Diagnostics.DebuggerDisplay("{Id,nq} - {LanguageCode,nq}")]
+public class LabelTranslation
+{
+    public string Id { get; set; } = default!;
+
+    public string LanguageCode { get; set; } = default!;
+
+    public string? Text { get; set; }
+}
+
+/// <summary> </summary>
+[System.Diagnostics.DebuggerDisplay("{Id,nq} - {DefaultValue,nq}")]
+public class TranslatableLabel
+{
+    public string Id { get; set; } = default!;
+
+    public string? DefaultValue { get; set; }
+
+    public List<LabelTranslation>? LabelTranslations { get; set; } = [];
+}
